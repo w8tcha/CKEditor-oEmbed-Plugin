6a3f73bec83e841902e3f8fd68c78375accddb79
Ingo Herbote <thewatcher@watchersnet.de>
2012-12-01 15:00:36 +0100

    updated

--- Plugin/sample/plugins/oEmbed/lang/de.js
+++
@@ -1,8 +0,0 @@
-CKEDITOR.plugins.setLang('oEmbed', 'de', {
-    title : "Medien Inhalt einbinden (Bilder, Video, Inhalt)",
-    button : "Medien Inhalt von Verschiedenen Seiten einbinden",
-    pasteUrl : "Fügen Sie eine Url (Gekürzte Urls werden auch erkannt) ein von einer Seite die Unterstützt wird (z.B.: YouTube, Flickr, Qik, Vimeo, Hulu, Viddler, MyOpera, etc.) ...",
-	width : "Maximale Breite:",
-	height : "Maximale Höhe:",
-	found : " gefunden!"
-});

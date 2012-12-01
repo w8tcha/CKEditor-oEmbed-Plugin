286c4f5a0284d410d3f01b0400227467af45a617
Ingo Herbote <thewatcher@watchersnet.de>
2012-12-01 14:56:38 +0100

    added sample pages

---
+++ Plugin/sample/plugins/oEmbed/lang/en.js
@@ -0,0 +1,8 @@
+CKEDITOR.plugins.setLang('oEmbed', 'en', {
+	title : "Embed Media Content (Photo, Video, Audio, Rich)",
+    button : "Embed Media Content from Various Sites",
+    pasteUrl : "Paste an URL (Also shortened URLs are supported) from one of the Supported Sites (e.g. YouTube, Flickr, Qik, Vimeo, Hulu, Viddler, MyOpera, etc.) here ...",
+	width : "Max. Width:",
+	height : "Max. Height:",
+	found : " found!"
+});
---
+++ Plugin/sample/plugins/oEmbed/plugin.js
@@ -0,0 +1,31 @@
+﻿/*
+* oEmbed Plugin plugin
+* Copyright (c) Ingo Herbote
+* Licensed under the MIT license
+* jQuery Embed Plugin Embeds: http://code.google.com/p/jquery-oembed/ (MIT License)
+* Plugin for: http://ckeditor.com/license (GPL/LGPL/MPL: http://ckeditor.com/license)
+*/
+
+(function () {
+    CKEDITOR.plugins.add('oEmbed', {
+        requires: ['dialog'],
+        lang: ['de', 'en','nl'],
+        init: function (editor) {
+			// Load jquery?
+			if (typeof(jQuery) == 'undefined') {
+				CKEDITOR.scriptLoader.load('http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js');
+				}
+				
+		    editor.addCommand('oEmbed', new CKEDITOR.dialogCommand('oEmbed'));
+            editor.ui.addButton('oEmbed', {
+                label: editor.lang.oEmbed.button,
+                command: 'oEmbed',
+                icon: this.path + 'images/icon.png'
+            });
+			
+            CKEDITOR.dialog.add('oEmbed', this.path + 'dialogs/oEmbedDialog.js');
+			
+			
+        }
+    })
+})();

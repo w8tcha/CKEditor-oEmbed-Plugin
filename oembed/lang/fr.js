4e07a632d56be288061e3a5a30a68c44dc2c27c2
wissim <christophe@wissim.fr>
2013-01-17 14:16:48 +0100

    Update oembed/lang/fr.js

--- oembed/lang/fr.js
+++ oembed/lang/fr.js
@@ -1,11 +1,11 @@
 // French Translation by https://github.com/wissim
 
 CKEDITOR.plugins.setLang('oembed', 'fr', {
-	title : "Intgrer des contenus multimédia externe. (Photo, Video, Audio, ...)",
-	button : "Insérer des contenus multimédia à partir de divers sites de contenu.",
-	pasteUrl : "Coller l'URL de partage que vous voulez publier. De nombreux services sont pris en charge (YouTube, Flickr, Qik, Vimeo, Hulu, Viddler, MyOpera, etc.). Vous pouvez aussi utiliser les URLs courtes.",
+	title : "Intégrer des contenus multimédia externes. (Photo, Video, Audio, ...)",
+	button : "Insérer des contenus multimédia provenant de nombreux sites.",
+	pasteUrl : "Coller l'URL de partage que vous voulez publier. De nombreux services sont pris en charge tels que : (YouTube, Flickr, Qik, Vimeo, Hulu, Viddler, MyOpera, etc.). Vous pouvez aussi utiliser les URLs courtes.",
 	width : "Max. Largeur:",
 	height : "Max. Hauteur:",
-	invalidUrl : "Merci de fournir une adresse valide !",
+	invalidUrl : "Merci de fournir une URL valide !",
 	noEmbedCode : "Aucun code d'intégration trouvé ou le site n'est pas supporté !"
 });

5cc7f218f45cd8527d9a1bf0f968dd675803a2db
Ingo Herbote <thewatcher@watchersnet.de>
2013-02-21 13:19:30 +0100

    also updated here

--- ckeditor/plugins/oembed/plugin.js
+++ ckeditor/plugins/oembed/plugin.js
@@ -6,35 +6,35 @@
 * Plugin for: http://ckeditor.com/license (GPL/LGPL/MPL: http://ckeditor.com/license)
 */
 
-(function () {
+(function() {
     CKEDITOR.plugins.add('oembed', {
         requires: ['dialog'],
         lang: ['de', 'en', 'nl', 'fr', 'ru'],
-        init: function (editor) {
+        init: function(editor) {
             // Load jquery?
-            if (typeof (jQuery) == 'undefined') {
-				CKEDITOR.scriptLoader.load('http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js', function() {
-					if (typeof (jQuery.fn.oembed) == 'undefined') {
-						CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(CKEDITOR.plugins.getPath('oembed') + 'libs/jquery.oembed.min.js'));
+            if (typeof(jQuery) == 'undefined') {
+                CKEDITOR.scriptLoader.load('http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js', function() {
+                    if (typeof(jQuery.fn.oembed) == 'undefined') {
+                        CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(CKEDITOR.plugins.getPath('oembed') + 'libs/jquery.oembed.min.js'));
                     }
-				});
+                });
 
-            } else if (typeof (jQuery.fn.oembed) == 'undefined') {
-				CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(CKEDITOR.plugins.getPath('oembed') + 'libs/jquery.oembed.min.js'));
+            } else if (typeof(jQuery.fn.oembed) == 'undefined') {
+                CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(CKEDITOR.plugins.getPath('oembed') + 'libs/jquery.oembed.min.js'));
             }
-			
+
             editor.addCommand('oembed', new CKEDITOR.dialogCommand('oembed'));
             editor.ui.addButton('oembed', {
                 label: editor.lang.oembed.button,
                 command: 'oembed',
                 icon: this.path + 'images/icon.png'
             });
-            CKEDITOR.dialog.add('oembed', function (editor) {
+            CKEDITOR.dialog.add('oembed', function(editor) {
                 return {
                     title: editor.lang.oembed.title,
                     minWidth: CKEDITOR.env.ie && CKEDITOR.env.quirks ? 568 : 550,
                     minHeight: 240,
-                    onOk: function () {
+                    onOk: function() {
                         var inputCode = this.getValueOf('general', 'embedCode').replace('https:', 'http:');
                         if (inputCode.length < 1 || inputCode.indexOf('http') < 0) {
                             alert(editor.lang.oembed.invalidUrl);
@@ -43,11 +43,10 @@
                         var width = this.getContentElement('general', 'width').getInputElement().getValue();
                         var height = this.getContentElement('general', 'height').getInputElement().getValue();
                         var editorInstance = this.getParentEditor();
-                        var codeFound = null;
                         $('body').oembed(inputCode, {
-                            onEmbed: function (e) {
-                                if (typeof e.code === 'string') { 
-								    editorInstance.insertElement( CKEDITOR.dom.element.createFromHtml( editor.config.oembed_WrapperClass != null ? '<div class="' + editor.config.oembed_WrapperClass + '">' : '<div>' + e.code + '</div>' ));
+                            onEmbed: function(e) {
+                                if (typeof e.code === 'string') {
+                                    editorInstance.insertElement(CKEDITOR.dom.element.createFromHtml((editor.config.oembed_WrapperClass != null ? '<div class="' + editor.config.oembed_WrapperClass + '">' : '<div>') + e.code + '</div>'));
                                     CKEDITOR.dialog.getCurrent().hide();
                                 } else {
                                     alert(editor.lang.oembed.noEmbedCode);
@@ -63,34 +62,34 @@
                         label: editor.lang.common.generalTab,
                         id: 'general',
                         elements: [{
-                            type: 'html',
-                            id: 'oembedHeader',
-                            html: '<div style="white-space:normal;width:500px;padding-bottom:10px">' + editor.lang.oembed.pasteUrl + '</div>'
-                        }, {
-                            type: 'textarea',
-                            id: 'embedCode',
-							height : "80",
-                            focus: function () {
-                                this.getElement().focus()
-                            }
-                        }, {
-                            type: 'hbox',
-                            widths: ['50%', '50%'],
-                            children: [{
-                                type: 'text',
-                                id: 'width',
-                                'default': editor.config.oembed_maxWidth != null ? editor.config.oembed_maxWidth : '560',
-                                label: editor.lang.oembed.width
+                                type: 'html',
+                                id: 'oembedHeader',
+                                html: '<div style="white-space:normal;width:500px;padding-bottom:10px">' + editor.lang.oembed.pasteUrl + '</div>'
                             }, {
-                                type: 'text',
-                                id: 'height',
-                                'default': editor.config.oembed_maxHeight != null ? editor.config.oembed_maxHeight : '315',
-                                label: editor.lang.oembed.height
+                                type: 'textarea',
+                                id: 'embedCode',
+                                height: "80",
+                                focus: function() {
+                                    this.getElement().focus();
+                                }
+                            }, {
+                                type: 'hbox',
+                                widths: ['50%', '50%'],
+                                children: [{
+                                        type: 'text',
+                                        id: 'width',
+                                        'default': editor.config.oembed_maxWidth != null ? editor.config.oembed_maxWidth : '560',
+                                        label: editor.lang.oembed.width
+                                    }, {
+                                        type: 'text',
+                                        id: 'height',
+                                        'default': editor.config.oembed_maxHeight != null ? editor.config.oembed_maxHeight : '315',
+                                        label: editor.lang.oembed.height
+                                    }]
                             }]
-                        }]
                     }]
-                }
-            })
+                };
+            });
         }
-    })
+    });
 })();

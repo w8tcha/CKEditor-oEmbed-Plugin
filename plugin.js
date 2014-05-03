7143d209b90971f449fce2dd98b9f9be6dac2b10
Ingo Herbote <thewatcher@watchersnet.de>
2014-05-03 17:52:31 +0200

    bump to 1.16

--- plugin.js
+++ plugin.js
@@ -11,7 +11,7 @@
         hidpi: true,
         requires: 'widget,dialog',
         lang: 'de,en,fr,nl,pl,pt-br,ru,tr', // %REMOVE_LINE_CORE%
-        version: 1.15,
+        version: 1.16,
         init: function (editor) {
             // Load jquery?
             loadjQueryLibaries();

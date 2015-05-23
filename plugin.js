5fb69d11d5757c10d32a61f5b53d884d27a96d75
Ingo Herbote <thewatcher@watchersnet.de>
2015-05-23 18:21:29 +0200

    sorted lang by alphabet

--- plugin.js
+++ plugin.js
@@ -10,7 +10,7 @@
             icons: 'oembed',
             hidpi: true,
             requires: 'widget,dialog',
-            lang: 'de,en,fr,nl,pl,pt-br,ru,tr,cs', // %REMOVE_LINE_CORE%
+            lang: 'cs,de,en,fr,nl,pl,pt-br,ru,tr', // %REMOVE_LINE_CORE%
             version: 1.17,
             init: function(editor) {
                 // Load jquery?
